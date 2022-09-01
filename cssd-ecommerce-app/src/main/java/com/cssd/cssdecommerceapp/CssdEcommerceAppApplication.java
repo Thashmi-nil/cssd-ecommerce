4b67e45f4fa096700eb179c6dfdbee2ced43c37f
Chathura <chathura.madura11@gmail.com>
2022-09-01 08:56:51 +0530

    initialize backend

---
+++ cssd-ecommerce-app/src/main/java/com/cssd/cssdecommerceapp/CssdEcommerceAppApplication.java
@@ -0,0 +1,14 @@
+package com.cssd.cssdecommerceapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CssdEcommerceAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CssdEcommerceAppApplication.class, args);
+		System.out.println("Running!");
+	}
+
+}
