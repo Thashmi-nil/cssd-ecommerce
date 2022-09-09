9114773c491df39ad37d1e535df149b87b992448
Chathura <chathura.madura11@gmail.com>
2022-09-10 02:54:20 +0530

    add to cart, remove from cart

--- cssd-ecommerce-app/src/main/java/com/cssd/cssdecommerceapp/controllers/CartController.java
+++ cssd-ecommerce-app/src/main/java/com/cssd/cssdecommerceapp/controllers/CartController.java
@@ -1,11 +1,33 @@
 package com.cssd.cssdecommerceapp.controllers;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.cssd.cssdecommerceapp.dto.CartDetails;
+import com.cssd.cssdecommerceapp.entities.Item;
+import com.cssd.cssdecommerceapp.services.CartService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1")
 @CrossOrigin(origins = "http://localhost:3000")
 public class CartController {
+    @Autowired
+    CartService cartService;
+
+    @GetMapping("/getCart/{id}")
+    public List<CartDetails> getCart(@PathVariable long id){
+        return cartService.getCart(id);
+    }
+
+    @PostMapping("/addCart")
+    public long addItem(Item item){
+        return cartService.addCart(item);
+    }
+
+    @DeleteMapping("/deleteCart/{id}")
+    public long deleteItem(@PathVariable long id){
+        return cartService.deleteCart(id);
+    }
+
 }
