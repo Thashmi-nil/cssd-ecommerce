06344e05f5059386f1fb03fa2f72f6b9d8c8e264
Deenath Geeganage <deenathgeeganage@gmail.com>
2022-09-09 13:44:29 +0530

    added Items entity

---
+++ cssd-ecommerce-app/src/main/java/com/cssd/cssdecommerceapp/entities/Items.java
@@ -0,0 +1,109 @@
+package com.cssd.cssdecommerceapp.entities;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import java.util.Collection;
+import java.util.Set;
+
+@Table(name="items")
+@Entity
+public class Items implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "itemID")
+    private long id;
+
+    @Column(name="itemName")
+    private String name;
+
+    @Column(name="itemType")
+    private String type;
+
+    @Column(name = "itemDescription")
+    private String description;
+
+    @Column(name = "itemImage")
+    private String image;
+
+    @OneToMany(mappedBy = "item")
+    Set<CartItems> carts;
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+}
