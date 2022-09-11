a5ac047a4d43717692400cf299d68491fd5ab34a
Chathura <chathura.madura11@gmail.com>
2022-09-11 13:48:52 +0530

    add to cart query

--- cssd-ecommerce-app/src/main/java/com/cssd/cssdecommerceapp/services/CartService.java
+++ cssd-ecommerce-app/src/main/java/com/cssd/cssdecommerceapp/services/CartService.java
@@ -1,7 +1,6 @@
 package com.cssd.cssdecommerceapp.services;
 
 import com.cssd.cssdecommerceapp.dto.CartDetails;
-import com.cssd.cssdecommerceapp.entities.Item;
 import com.cssd.cssdecommerceapp.repository.cartDao.CartJdbcRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,15 +16,32 @@ public class CartService {
         return cartJdbcRepository.getCart(id);
     }
 
-    public long addCart(Item item) {
-//        ToDo : if item already in the cart with particular customer then increment quantity
+    public long addCart(long item, long customerId) {
+        System.out.println("item->"+item);
+        long existsInCart = cartJdbcRepository.checkExists(customerId);
+        if(existsInCart!=0){
+            System.out.println("existsInCart->"+existsInCart);
 
-//        ToDo : else add new to cart table and add cart items
-        return cartJdbcRepository.addCart(item);
+            long existsInCartItem = cartJdbcRepository.checkExistsInCartItem(existsInCart,item);
+            if(existsInCartItem!=0){
+                System.out.println("existsInCartItem->"+existsInCartItem);
+                return cartJdbcRepository.increaseCartItemCount(item,existsInCart);
+            }else{
+//              ToDo : if not exists existsInCartItem should return 0!
+                return cartJdbcRepository.addCartItem(item,existsInCart);
+            }
+        }else{
+//            ToDo : if not exists existsInCart should return 0!
+            return cartJdbcRepository.addCart(item,customerId);
+        }
+
     }
 
-    public long deleteCart(long id) {
-//        ToDo : remove from cart item or decrease quantity count
-        return cartJdbcRepository.deleteCart(id);
+    public long deleteCart(long itemId, long customerId) {
+        System.out.println("itemId->"+itemId);
+        long existsInCart = cartJdbcRepository.checkExists(customerId);
+            System.out.println("existsInCart->"+existsInCart);
+            return cartJdbcRepository.decreaseCartItemCount(itemId,existsInCart);
+
     }
 }
